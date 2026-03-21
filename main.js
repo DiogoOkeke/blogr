4b705ad84b2d263cb06388a20cf0165fe75c4a57
Okeke Chidiogo <okekechidiogoc@gmail.com>
2026-03-21 08:13:04 +0100

    made changes to style.css

--- main.js
+++ main.js
@@ -10,23 +10,10 @@ let navTwo = document.querySelector(".navTwo");
 
 // adding listening event to the hamburger menu
 
-let clicked = false;
-
 hamburgerMenu.addEventListener("click", () => {
-  if (!clicked) {
-    firstBar.classList.add("true");
-    secondBar.classList.add("true");
-    thirdBar.classList.add("true");
-    navTwo.classList.add("true");
-    clicked = true;
-    console.log(" diogo");
-  } else {
-    firstBar.classList.remove("true");
-    secon
-    dBar.classList.remove("true");
-    thirdBar.classList.remove("true");
-    navTwo.classList.remove("true");
-    clicked = false;
-    console.log("ndaeyo");
-  }
+  firstBar.classList.toggle("true");
+  secondBar.classList.toggle("true");
+  thirdBar.classList.toggle("true");
+  navTwo.classList.toggle("true");
+  console.log(" hamburger clicked");
 });
